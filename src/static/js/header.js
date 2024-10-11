2bdb3068a2ac8e9b92defe4c58fce812d7249546
fulbertmissekpe <fulbert.missekpe@ism.edu.sn>
2024-10-11 18:15:45 +0000

    UPDATE FIN ACCUEIL H and FT
    Responsive header footer et fin accueil

--- src/static/js/header.js
+++ src/static/js/header.js
@@ -1,8 +1,8 @@
 const headerHTML = `  
 <header class=" sticky top-0 z-10 ">
     <div class="w-full text-white bg-primary ">
-        <div class="mx-auto max-w-screen-lg flex justify-between text-sm">
-            <div class="w-auto flex justify-start">
+        <div class="mx-auto max-w-screen-lg md:flex justify-between hidden  text-sm">
+            <div class="w-auto flex justify-start ">
                 <div class="flex justify-center items-center py-3 mr-10">
                     <svg class="h-5 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
@@ -50,8 +50,21 @@ const headerHTML = `
                 <img class="w-12 mr-2" src="../static/images/logo.png" alt="">
                 <h2 class=" text-2xl font-semibold">Sakku Yiw</h2>
             </div>
+            <div class=" md:hidden items-center justify-between p-5">  
+            <button id="burger" class="focus:outline-none">  
+                <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">  
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>  
+                </svg>  
+            </button>  
+            </div>  
 
-            <div class="flex items-center text-sm ">
+            <div id="menu" class="menu-transition transform -translate-y-full bg-white shadow-md rounded-lg p-5 absolute right-5 w-48 hidden">  
+                <a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Home</a>  
+                <a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">About</a>  
+                <a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Services</a>  
+                <a href="#" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">Contact</a>  
+            </div>  
+            <div class="hidden md:flex items-center text-sm ">
                 <a href="#" class="mr-5">À propos</a>
                 <a href="#" class="mr-5 flex items-center ">
                     <span> Projets</span>
@@ -64,7 +77,7 @@ const headerHTML = `
                 <a href="#">Contact</a>
             </div>
 
-            <div class="flex items-center text-sm ">
+            <div class="hidden  md:flex items-center text-sm ">
                 <a href="#" class="mr-4 border border-white rounded p-3 focus:ring-4 focus:ring-secondary hover:bg-secondary hover:border-secondary">Donnez Votre Zakat</a>
                 <a href="#" class="flex px-6 items-center border border-secondary bg-secondary rounded py-3 focus:ring-4 focus:ring-amber-600 hover:bg-amber-600 hover:border-amber-600">
                     <p>Faire un Don</p>
